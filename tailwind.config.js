43ac7696c96e7a3df28202b1dea3136f308dcb26
michailtjhang <michailaja@gmail.com>
2024-07-03 09:24:36 +0700

    refactor: remove styles.css for github

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,php}", "./**/admin/**.{html,js,php}"],
+  content: ["**/*.{html,js,php}", "**/admin/**.{html,js,php}"],
   theme: {
     extend: {
       colors: {
